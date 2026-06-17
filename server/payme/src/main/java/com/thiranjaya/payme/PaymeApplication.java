a3dd9bf05cf939e58eb1f74c78a16c14d529c318
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-06-17 19:34:54 +0530

    feat: setup dotenv for the project

--- server/payme/src/main/java/com/thiranjaya/payme/PaymeApplication.java
+++ server/payme/src/main/java/com/thiranjaya/payme/PaymeApplication.java
@@ -2,11 +2,19 @@ package com.thiranjaya.payme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PaymeApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+	
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(PaymeApplication.class, args);
 	}
 
