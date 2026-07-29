2aa708abf665742429fbbe0767c1eeb52d85ee36
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-07-29 09:40:50 +0530

    feat: server user initilization

---
+++ server/server_user/src/main/java/com/payme/server_user/ServerUserApplication.java
@@ -0,0 +1,13 @@
+package com.payme.server_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerUserApplication.class, args);
+	}
+
+}
