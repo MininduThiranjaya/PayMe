c1560950dbbc67eb581bced5cc757dd75807a8da
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-07-29 13:47:27 +0530

    feat: create user model

---
+++ server/server_user/src/main/java/com/payme/server_user/model/UserModel.java
@@ -0,0 +1,64 @@
+package com.payme.server_user.model;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class UserModel {
+
+    public enum Role {
+        CUSTOMER,
+        MERCHANT
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "username", nullable = false, unique = false)
+    private String userName;
+    @Column(name = "password", nullable = false, unique = false)
+    private String password;
+    @Column(name = "nic", nullable = false, unique = true)
+    private String nic;
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, unique = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false, unique = false)
+    private LocalDateTime updatedAt;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "nic")
+    )
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+}
