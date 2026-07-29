9cbcadd604d499fe329e36398999339c15778d1d
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-07-29 15:09:14 +0530

    feat: create user registration

--- server/server_user/src/main/java/com/payme/server_user/controller/UserController.java
+++ server/server_user/src/main/java/com/payme/server_user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.payme.server_user.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.payme.server_user.DTO.req_dto.userReg_req_dto;
+import com.payme.server_user.DTO.req_dto.UserReg_req_dto;
+import com.payme.server_user.DTO.res_dto.UserReg_res_dto;
 import com.payme.server_user.services.UserService;
 
 import jakarta.validation.Valid;
@@ -20,9 +22,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("reg")
-    public String regUserControl(@Valid @RequestBody userReg_req_dto data) {
-        return userService.registerUserService(data);
+    public ResponseEntity<UserReg_res_dto> regUserControl(@Valid @RequestBody UserReg_req_dto data) {
+        UserReg_res_dto savedUser =  userService.registerUserService(data);
+        return ResponseEntity.ok(savedUser);
     }
-    
-
 }
--- server/server_user/src/main/java/com/payme/server_user/services/UserService.java
+++ server/server_user/src/main/java/com/payme/server_user/services/UserService.java
@@ -1,8 +1,14 @@
 package com.payme.server_user.services;
 
+import java.util.Set;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.payme.server_user.DTO.req_dto.userReg_req_dto;
+import com.payme.server_user.DTO.req_dto.UserReg_req_dto;
+import com.payme.server_user.DTO.res_dto.UserReg_res_dto;
+import com.payme.server_user.error.exceptions.UserAlreadyExistsExc;
+import com.payme.server_user.model.UserModel;
 import com.payme.server_user.repository.UserRepo;
 
 import lombok.RequiredArgsConstructor;
@@ -12,8 +18,28 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
     
-    public String registerUserService(userReg_req_dto data) {
-        return "done";
+    public UserReg_res_dto registerUserService(UserReg_req_dto data) {
+
+        if(userRepo.userExistByNIC(data.getNic())) {
+            throw new UserAlreadyExistsExc(data.getNic());
+        }
+
+        UserModel user = new UserModel();
+        UserModel.Role role = UserModel.Role.valueOf(data.getRole());
+        
+        user.setNic(data.getNic());
+        user.setUserName(data.getUserName());
+        user.setPassword(passwordEncoder.encode(data.getPassword()));
+        user.setRoles(Set.of(role));
+
+        UserModel savedUser = userRepo.save(user);
+
+        return new UserReg_res_dto(
+            savedUser.getNic(),
+            savedUser.getUserName(),
+            savedUser.getRoles()
+        );
     }
 }
