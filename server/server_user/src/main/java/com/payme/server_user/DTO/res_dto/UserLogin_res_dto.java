d0b97490113a4663af3770609974e1fc5f50d0be
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-07-31 23:49:28 +0530

    feat: login res dto

---
+++ server/server_user/src/main/java/com/payme/server_user/DTO/res_dto/UserLogin_res_dto.java
@@ -0,0 +1,18 @@
+package com.payme.server_user.DTO.res_dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLogin_res_dto {
+    
+    String token;
+    private long expiresIn;
+    private Object user;
+    private String roles;
+}
