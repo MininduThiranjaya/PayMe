7265fefbc1fcec9162477e8df717cb489082d289
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-07-31 23:49:59 +0530

    feat: add function for findByNic

--- server/server_user/src/main/java/com/payme/server_user/repository/UserRepo.java
+++ server/server_user/src/main/java/com/payme/server_user/repository/UserRepo.java
@@ -1,5 +1,7 @@
 package com.payme.server_user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.payme.server_user.model.UserModel;
@@ -7,4 +9,5 @@ import com.payme.server_user.model.UserModel;
 public interface  UserRepo extends JpaRepository<UserModel, Long> {
 
     Boolean existsByNic(String nic);
+    Optional<UserModel> findByNic(String nic);
 }
