29d0a85c9c892cc3c349bba75bc8c5530fe51132
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-01 10:32:46 +0530

    feat: add merchant-reg crontroller and rename previous controller into user-reg

--- server/server_user/src/main/java/com/payme/server_user/controller/UserController.java
+++ server/server_user/src/main/java/com/payme/server_user/controller/UserController.java
@@ -1,20 +1,22 @@
 package com.payme.server_user.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.payme.server_user.DTO.req_dto.MerchantReg_req_dto;
 import com.payme.server_user.DTO.req_dto.UserLogin_req_dto;
 import com.payme.server_user.DTO.req_dto.UserReg_req_dto;
+import com.payme.server_user.DTO.res_dto.MerchantReg_res_dto;
 import com.payme.server_user.DTO.res_dto.UserLogin_res_dto;
 import com.payme.server_user.DTO.res_dto.UserReg_res_dto;
 import com.payme.server_user.services.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
@@ -25,10 +27,17 @@ public class UserController {
     private final UserService userService;
 
 
-    @PostMapping("/reg")
+    @PostMapping("/user-reg")
     public ResponseEntity<UserReg_res_dto> regUserControl(@Valid @RequestBody UserReg_req_dto data) {
         
         UserReg_res_dto savedUser =  userService.registerUserService(data);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @PostMapping("/merchant-reg")
+    public ResponseEntity<MerchantReg_res_dto> regMerchantControl(@Valid @RequestBody MerchantReg_req_dto data) {
+        
+        MerchantReg_res_dto savedUser =  userService.registerMerchantService(data);
         return ResponseEntity.ok(savedUser);
     }
 
