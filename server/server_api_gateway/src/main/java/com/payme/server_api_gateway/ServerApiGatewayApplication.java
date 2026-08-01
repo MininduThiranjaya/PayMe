633125b5a40b5b5fa220fab52e3575109cfe873a
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-01 13:27:11 +0530

    feat: initialize second server server_api_gateway

---
+++ server/server_api_gateway/src/main/java/com/payme/server_api_gateway/ServerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.payme.server_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApiGatewayApplication.class, args);
+	}
+
+}
---
+++ server/server_api_gateway/src/test/java/com/payme/server_api_gateway/ServerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payme.server_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
