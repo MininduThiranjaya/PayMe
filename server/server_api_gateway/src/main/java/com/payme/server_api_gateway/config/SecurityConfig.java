b9b3fa2471bd5bb77bc14edb2bb9c1b49f64f17e
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-02 00:05:00 +0530

    feat: setup security config  for permit any exchange

---
+++ server/server_api_gateway/src/main/java/com/payme/server_api_gateway/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.payme.server_api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeExchange(auth -> auth.anyExchange().permitAll())
+            .build();
+    }
+}
