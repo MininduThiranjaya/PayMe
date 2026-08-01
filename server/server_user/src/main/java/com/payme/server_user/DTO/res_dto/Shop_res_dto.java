f3291baaa1310e0c22d5e41928292c67b714da0c
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-01 10:33:32 +0530

    feat: shop res dto

---
+++ server/server_user/src/main/java/com/payme/server_user/DTO/res_dto/Shop_res_dto.java
@@ -0,0 +1,14 @@
+package com.payme.server_user.DTO.res_dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Shop_res_dto {
+
+    private String shopName;
+    private String address;
+}
