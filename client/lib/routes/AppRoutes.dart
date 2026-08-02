d4c8f103d2678d95677a069dd5a4e04ef84948a0
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-03 00:08:53 +0530

    feat: add app routes with auth guard

--- client/lib/routes/AppRoutes.dart
+++ client/lib/routes/AppRoutes.dart
@@ -1,3 +1,5 @@
+import 'package:client/guard/AuthGuard.dart';
+import 'package:client/screens/common/UserDashboard_Screen.dart';
 import 'package:client/screens/splash_screen/Splash_Screen.dart';
 import 'package:client/screens/common/introScreens/Intro_Screen.dart';
 import 'package:client/screens/common/Login_Screen.dart';
@@ -8,5 +10,9 @@ class Approutes {
     "/": (context) =>  const Splash_Screen(),
     "/introScreen": (context) =>  const Intro_Screen(),
     "/loginScreen": (context) =>  const Login_Screen(),
+    // auth
+    '/auth': (context) => const AuthGuard(child: UserDashboard_Screen()),
+    // auth routes
+    // "/dashboard": (context) => const AuthGuard(child: UserDashboard_Screen())
   };
 }
