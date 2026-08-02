312d9e0ceaecbd1a6db519c5835ffc8965ac278a
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-02 13:06:07 +0530

    feat: add onboard storage service

---
+++ client/lib/services/Onboard_Storage_Service.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class OnboardStorageService {
+
+  final String onboardedKey = "onboarded";
+
+  final SharedPreferencesAsync preferences = SharedPreferencesAsync();
+
+  Future<bool> hasSeenOnboarding() async {
+    return await preferences.getBool(onboardedKey) ?? false;
+  }
+
+  Future<void> completeOnboarding() async {
+    return await preferences.setBool(onboardedKey, true);
+  }
+
+  Future<void> resetOnboarding() async {
+    return await preferences.setBool(onboardedKey, false);
+  }
+}
