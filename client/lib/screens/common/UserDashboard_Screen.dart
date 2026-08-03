ee6cff561f250aca6d6816e874867cc21b3f078f
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-03 18:09:02 +0530

    feat: modified common user dashbord to ristrict users by role and route redirecting

--- client/lib/screens/common/UserDashboard_Screen.dart
+++ client/lib/screens/common/UserDashboard_Screen.dart
@@ -1,42 +1,26 @@
+import 'package:client/providers/AuthProvider.dart';
+import 'package:client/screens/common/RoleSelection_Screen.dart';
+import 'package:client/screens/customer/CustomerDashboard_Screen.dart';
+import 'package:client/screens/merchant/MerchantDashboard_Screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class UserDashboard_Screen extends StatefulWidget {
+class UserDashboard_Screen extends StatelessWidget {
+
   const UserDashboard_Screen({super.key});
   @override
-  State<UserDashboard_Screen> createState() => _UserDashboard_Screen_State();
-}
-
-class _UserDashboard_Screen_State extends State<UserDashboard_Screen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            const pagePadding = EdgeInsets.all(20);
-            final availableHeight =
-                constraints.maxHeight - pagePadding.vertical;
 
-            return SingleChildScrollView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              child: Padding(
-                padding: pagePadding,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(minHeight: availableHeight),
-                  child: IntrinsicHeight(
-                    child: Column(
-                      children: [
-                        Text('Dashboard')
-                      ]
-                    )
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+    final activeRole = context.select<AuthProvider, String?>(
+      (provider) => provider.activeRole
     );
+    switch(activeRole) {
+      case 'CUSTOMER':
+        return CustomerDashboard_Screen();
+      case 'MERCHANT':
+        return MerchantDashboard_Screen();
+      default:
+        return RoleSelection_Screen();
+    }
   }
 }
