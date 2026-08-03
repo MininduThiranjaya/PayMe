395c6327516bc3771074cb2b559c8758ed620975
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-03 17:53:44 +0530

    feat: add merchant dashboard

---
+++ client/lib/screens/merchant/MerchantDashboard_Screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MerchantDashboard_Screen extends StatefulWidget {
+  const MerchantDashboard_Screen({super.key});
+  @override
+  State<MerchantDashboard_Screen> createState() => _MerchantDashboard_Screen_State();
+}
+
+class _MerchantDashboard_Screen_State extends State<MerchantDashboard_Screen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            const pagePadding = EdgeInsets.all(20);
+            final availableHeight =
+                constraints.maxHeight - pagePadding.vertical;
+
+            return SingleChildScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              child: Padding(
+                padding: pagePadding,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: availableHeight),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        Text('Merchant dashboard')
+                      ]
+                    )
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
