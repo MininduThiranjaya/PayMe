65dfe5ee4509f57b9a888b6ebc4714ac71662f5f
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-03 18:03:35 +0530

    feat: add role storage with notification provider with lazy -> false

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:client/config/DioClient.dart';
 import 'package:client/providers/AuthProvider.dart';
 import 'package:client/services/Login_Service.dart';
+import 'package:client/storage/Role_Storage.dart';
 import 'package:client/storage/Token_Storage.dart';
 import 'package:flutter/material.dart';
 import 'package:client/routes/AppRoutes.dart';
@@ -11,9 +12,11 @@ void main() {
   final dio = DioClient();
   runApp(
     ChangeNotifierProvider(
+      lazy: false,
       create: (_) => AuthProvider(
         loginService: Login_Service(dioClient: dio),
         tokenStorage: Token_Storage(),
+        roleStorage: Role_Storage()
       )..getMe(),
       child: const MyApp()
     )  
