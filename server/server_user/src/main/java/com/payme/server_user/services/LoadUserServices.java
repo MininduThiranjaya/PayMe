13890584e48f192c16db25c4f94c79911080fe85
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-03 07:57:09 +0530

    fix: resolve throwing generic exception class

--- server/server_user/src/main/java/com/payme/server_user/services/LoadUserServices.java
+++ server/server_user/src/main/java/com/payme/server_user/services/LoadUserServices.java
@@ -2,10 +2,10 @@ package com.payme.server_user.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.payme.security.AppUserDetails;
-import com.payme.server_user.error.exceptions.UserNotFoundExc;
 import com.payme.server_user.model.UserModel;
 import com.payme.server_user.repository.UserRepo;
 
@@ -18,10 +18,10 @@ public class LoadUserServices implements UserDetailsService {
     private final UserRepo userRepo;
     
     @Override
-    public UserDetails loadUserByUsername(String nic) throws UserNotFoundExc {
+    public UserDetails loadUserByUsername(String nic) throws UsernameNotFoundException {
 
         UserModel user = userRepo.findByNic(nic)
-            .orElseThrow(() -> new UserNotFoundExc(nic));
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + nic));
         return new AppUserDetails(user);
     }
 }
