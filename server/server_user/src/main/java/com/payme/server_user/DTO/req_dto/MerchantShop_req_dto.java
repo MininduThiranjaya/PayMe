8da390355ba87ace3f2d9c10957612344cfa689a
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-05 00:38:17 +0530

    feat: add separate merchant shop dto only for role update to merchant with shop details

---
+++ server/server_user/src/main/java/com/payme/server_user/DTO/req_dto/MerchantShop_req_dto.java
@@ -0,0 +1,27 @@
+package com.payme.server_user.DTO.req_dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MerchantShop_req_dto {
+    
+     @NotEmpty(message = "At least one shop name is required")
+    private List<
+        @NotBlank(message = "Shop name cannot be blank")
+        String
+    > shopNames = new ArrayList<>();
+    @NotEmpty(message = "At least one shop address is required")
+    private List<
+        @NotBlank(message = "Shop address cannot be blank")
+        String
+    > addresses = new ArrayList<>();
+}
