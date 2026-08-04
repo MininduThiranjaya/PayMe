423a0cf5e48395e27e2c346677eddb55e006d778
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-05 00:37:31 +0530

    feat: add new routes for update user role and modify the route path

--- server/server_user/src/main/java/com/payme/server_user/controller/UserController.java
+++ server/server_user/src/main/java/com/payme/server_user/controller/UserController.java
@@ -4,12 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payme.security.AppUserDetails;
 import com.payme.server_user.DTO.req_dto.MerchantReg_req_dto;
+import com.payme.server_user.DTO.req_dto.MerchantShop_req_dto;
 import com.payme.server_user.DTO.req_dto.UserLogin_req_dto;
 import com.payme.server_user.DTO.req_dto.UserReg_req_dto;
 import com.payme.server_user.DTO.res_dto.CurrentUserProfile_res_dto;
@@ -30,14 +32,14 @@ public class UserController {
     private final UserService userService;
 
 
-    @PostMapping("/user-reg")
+    @PostMapping("/reg/customer")
     public ResponseEntity<UserReg_res_dto> regUserControl(@Valid @RequestBody UserReg_req_dto data) {
         
-        UserReg_res_dto savedUser =  userService.registerUserService(data);
+        UserReg_res_dto savedUser =  userService.registerCustomerService(data);
         return ResponseEntity.ok(savedUser);
     }
 
-    @PostMapping("/merchant-reg")
+    @PostMapping("/reg/merchant")
     public ResponseEntity<MerchantReg_res_dto> regMerchantControl(@Valid @RequestBody MerchantReg_req_dto data) {
         
         MerchantReg_res_dto savedUser =  userService.registerMerchantService(data);
@@ -56,6 +58,20 @@ public class UserController {
         
         CurrentUserProfile_res_dto currentUserDetails = userService.getCurrentUserDetailsService(currentUser.getUsername());
         return ResponseEntity.ok(currentUserDetails);
+    }
+
+    @PutMapping("/update/role-merchant")
+    public ResponseEntity<CurrentUserProfile_res_dto> updateUserRoleToMerchantController(@AuthenticationPrincipal AppUserDetails currentUser, @Valid @RequestBody MerchantShop_req_dto data) {
+
+        CurrentUserProfile_res_dto updatedUserCustomer = userService.updateUserRoleToMerchantService(currentUser.getUsername(), data);
+        return ResponseEntity.ok(updatedUserCustomer);
+    }
+
+    @PutMapping("/update/role-customer")
+    public ResponseEntity<CurrentUserProfile_res_dto> updateUserRoleToCustomerController(@AuthenticationPrincipal AppUserDetails currentUser) {
+
+        CurrentUserProfile_res_dto updatedUserMerchant = userService.updateUserRoleToCustomerService(currentUser.getUsername());
+        return ResponseEntity.ok(updatedUserMerchant);
     }
 
     @GetMapping("/test")
