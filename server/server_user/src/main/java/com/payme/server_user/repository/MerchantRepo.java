734f73d36362dd505ba6c08a52066c13cdb910a2
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-05 00:39:30 +0530

    feat: add query for add merchant by using user id

--- server/server_user/src/main/java/com/payme/server_user/repository/MerchantRepo.java
+++ server/server_user/src/main/java/com/payme/server_user/repository/MerchantRepo.java
@@ -1,9 +1,22 @@
 package com.payme.server_user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.payme.server_user.model.MerchantModel;
 
+
 public interface MerchantRepo extends JpaRepository<MerchantModel, Long> {
     
+    @Modifying
+    @Query(
+        value = """
+            INSERT INTO merchant_details (id)
+            VALUES (:userId)
+            ON CONFLICT (id) DO NOTHING
+                """,
+                nativeQuery=true
+    ) int createMerchantRecord(@Param("userId") long userId);
 }
