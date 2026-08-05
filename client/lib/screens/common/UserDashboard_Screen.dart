70c45cc182075b2b9666f0f7575acb8cfe09d164
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-05 22:44:34 +0530

    fix: set up navigation via routes than call directly to the screen

--- client/lib/screens/common/UserDashboard_Screen.dart
+++ client/lib/screens/common/UserDashboard_Screen.dart
@@ -1,26 +1,58 @@
 import 'package:client/providers/AuthProvider.dart';
-import 'package:client/screens/common/RoleSelection_Screen.dart';
-import 'package:client/screens/customer/CustomerDashboard_Screen.dart';
-import 'package:client/screens/merchant/MerchantDashboard_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class UserDashboard_Screen extends StatelessWidget {
-
+class UserDashboard_Screen extends StatefulWidget {
   const UserDashboard_Screen({super.key});
+
+  @override
+  State<UserDashboard_Screen> createState() =>
+      _UserDashboard_ScreenState();
+}
+
+class _UserDashboard_ScreenState extends State<UserDashboard_Screen> {
+  String? _scheduledRoute;
+
   @override
   Widget build(BuildContext context) {
-
     final activeRole = context.select<AuthProvider, String?>(
-      (provider) => provider.activeRole
+      (provider) => provider.activeRole,
     );
-    switch(activeRole) {
+
+    final String targetRoute;
+
+    switch (activeRole) {
       case 'CUSTOMER':
-        return CustomerDashboard_Screen();
+        targetRoute = '/customer/dashboard';
+        break;
+
       case 'MERCHANT':
-        return MerchantDashboard_Screen();
+        targetRoute = '/merchant/dashboard';
+        break;
+
       default:
-        return RoleSelection_Screen();
+        targetRoute = '/select/role';
     }
+
+    if (_scheduledRoute != targetRoute) {
+      _scheduledRoute = targetRoute;
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (!mounted) {
+          return;
+        }
+
+        Navigator.pushReplacementNamed(
+          context,
+          targetRoute,
+        );
+      });
+    }
+
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
