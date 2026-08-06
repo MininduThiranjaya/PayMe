14cea54a2ba20e6a1ede4c5b2e33c76d73783be2
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-06 09:14:36 +0530

    feat: initiate server order

---
+++ server/server_order/src/main/java/com/payme/server_order/ServerOrderApplication.java
@@ -0,0 +1,13 @@
+package com.payme.server_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerOrderApplication.class, args);
+	}
+
+}
