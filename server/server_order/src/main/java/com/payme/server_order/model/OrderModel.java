331a0b636ae0edde6f3b773c79840934213b7bb6
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-13 10:53:16 +0530

    feat: add order model

---
+++ server/server_order/src/main/java/com/payme/server_order/model/OrderModel.java
@@ -0,0 +1,46 @@
+package com.payme.server_order.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+@NoArgsConstructor
+class OrderModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "merchantid", nullable = false)
+    private long merchatId;
+    @Column(name = "customerid", nullable = false)
+    private long customerId;
+    @OneToMany(
+        mappedBy = "order_item",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
+    private List<OrderItemModel> orderItem = new ArrayList<>();
+    @Column(name = "created_at", nullable = false, unique = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false, unique = false)
+    private LocalDateTime updatedAt;
+}
