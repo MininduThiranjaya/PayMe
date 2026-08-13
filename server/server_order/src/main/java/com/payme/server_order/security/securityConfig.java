05c0860cff21565d29666baab411c6265c9ab9df
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-13 16:16:16 +0530

    feat: add security config to the server make the endpoint allows

--- server/server_order/src/main/java/com/payme/server_order/security/securityConfig.java
+++ server/server_order/src/main/java/com/payme/server_order/security/securityConfig.java
@@ -1,5 +1,24 @@
 package com.payme.server_order.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class securityConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth ->
+                auth
+                    .requestMatchers(HttpMethod.POST, "/payme/api/order/set-new-order").permitAll()
+                    .anyRequest().authenticated()
+            );
+        return http.build();
+    }
 }
