81b4c7dd323673f3d9804c4b2bde10e117daad98
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-15 12:14:56 +0530

    feat: add order service

--- server/server_order/src/main/java/com/payme/server_order/service/OrderService.java
+++ server/server_order/src/main/java/com/payme/server_order/service/OrderService.java
@@ -2,7 +2,12 @@ package com.payme.server_order.service;
 
 import org.springframework.stereotype.Service;
 
+import com.payme.server_order.DTO.req_dto.NewOrder_req_dto;
+
 @Service
 public class OrderService {
     
+    public String setNewOrderService(NewOrder_req_dto data) {
+        return "test";
+    }
 }
