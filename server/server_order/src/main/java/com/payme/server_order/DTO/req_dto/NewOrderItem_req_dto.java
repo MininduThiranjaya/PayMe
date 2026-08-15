3c7aadb12801438afb17f444f0eddd575afdbd1c
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-15 12:14:38 +0530

    feat: order item req dto

--- server/server_order/src/main/java/com/payme/server_order/DTO/req_dto/NewOrderItem_req_dto.java
+++ server/server_order/src/main/java/com/payme/server_order/DTO/req_dto/NewOrderItem_req_dto.java
@@ -3,6 +3,7 @@ package com.payme.server_order.DTO.req_dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
@@ -15,8 +16,12 @@ public class NewOrderItem_req_dto {
         G
     }
     
+    @NotEmpty(message = "Item name is required")
     private String itemName;
+    @NotEmpty(message = "Quantity is required")
     private int quantity;
+    @NotEmpty(message = "Item metric type is required")
     private String ItemMetric;
+    @NotEmpty(message = "Unit price is required")
     private int unitPrice;
 }
