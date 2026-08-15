809cf45cbc5155ab351b03dde7147c0f9c7d9214
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-08-15 12:14:08 +0530

    feat: add order controller

--- server/server_order/src/main/java/com/payme/server_order/controller/OrderControler.java
+++ server/server_order/src/main/java/com/payme/server_order/controller/OrderControler.java
@@ -1,16 +1,15 @@
 package com.payme.server_order.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
 
+import com.payme.server_order.DTO.req_dto.NewOrder_req_dto;
 import com.payme.server_order.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.payme.server_order.DTO.req_dto.NewOrder_req_dto;
 
 
 @RestController
@@ -21,8 +20,9 @@ public class OrderControler {
     private final OrderService orderService;
 
     @PostMapping("/set-new-order")
-    public String setNewOrder(@RequestBody NewOrder_req_dto data) {        
-        return "entity";
+    public String setNewOrderController(@Valid @RequestBody NewOrder_req_dto data) {        
+        String response = orderService.setNewOrderService(data);
+        return response;
     }
     
 }
