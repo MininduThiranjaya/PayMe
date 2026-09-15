db959272e805eddd4c4fe8f0fd14c732b2b3e0fb
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-15 16:11:45 +0530

    feat: add additional attributes to res dto

--- server/server_payment/src/main/java/com/payme/server_payment/DTO/res_dto/RegStripeConnectAcc_res_dto.java
+++ server/server_payment/src/main/java/com/payme/server_payment/DTO/res_dto/RegStripeConnectAcc_res_dto.java
@@ -2,13 +2,20 @@ package com.payme.server_payment.DTO.res_dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter 
 @Builder
 public class RegStripeConnectAcc_res_dto {
     
-    private String stripId;
+    private String stripeId;
     private String stripeOnboardingURL;
+    private Boolean chargesEnabled;
+    private Boolean payoutsEnabled;
+    private Boolean detailsSubmitted;
 }
