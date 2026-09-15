7196db6b605ee4368a10046d347c13f3e3f9d245
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-15 16:10:33 +0530

    feat: server security config

---
+++ server/server_payment/src/main/java/com/payme/server_payment/config/securityConfig.java
@@ -0,0 +1,81 @@
+package com.payme.server_payment.config;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.jwt.JwtClaimValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import io.jsonwebtoken.security.Keys;
+
+
+@Configuration
+@EnableMethodSecurity
+public class securityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth ->
+                auth
+                    // .requestMatchers(HttpMethod.POST, "/payme/api/order/set-new-order").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/payme/api/payment/reg/stripe-connect-acc").permitAll()
+                    .anyRequest().authenticated()
+            ).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> 
+                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+            ));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(@Value("${JWT_SECRET}") String secretKey) {
+        
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        NimbusJwtDecoder decoder = NimbusJwtDecoder
+            .withSecretKey(key)
+            .macAlgorithm(MacAlgorithm.HS256)
+            .build();
+        OAuth2TokenValidator<Jwt> issuerValidator = JwtValidators.createDefaultWithIssuer("payme-auth");
+        OAuth2TokenValidator<Jwt> audienceValidator = new JwtClaimValidator<List<String>>(JwtClaimNames.AUD, audience -> audience != null && audience.contains("payme-api"));
+        decoder.setJwtValidator(
+            new DelegatingOAuth2TokenValidator<>(
+                issuerValidator,
+                audienceValidator
+            )
+        );
+        return decoder;
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(
+            authoritiesConverter
+        );
+        return converter;
+    }
+}
