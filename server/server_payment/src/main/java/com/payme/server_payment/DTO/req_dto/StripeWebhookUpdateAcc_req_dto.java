56514c070ff4f1875f6a58f175cf36ad7713c888
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-17 18:19:59 +0530

    feat: add stripe acc update dto

---
+++ server/server_payment/src/main/java/com/payme/server_payment/DTO/req_dto/StripeWebhookUpdateAcc_req_dto.java
@@ -0,0 +1,19 @@
+package com.payme.server_payment.DTO.req_dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class StripeWebhookUpdateAcc_req_dto {
+    
+    private String stripeId;
+    private Boolean chargesEnabled;
+    private Boolean payoutsEnabled;
+    private Boolean detailsSubmitted;
+}
