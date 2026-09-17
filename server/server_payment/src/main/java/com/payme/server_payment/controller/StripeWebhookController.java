df5b41441ddf1cbf524f0af7300694cf26145eda
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-17 18:19:22 +0530

    feat: add stripe webhook controller

---
+++ server/server_payment/src/main/java/com/payme/server_payment/controller/StripeWebhookController.java
@@ -0,0 +1,24 @@
+package com.payme.server_payment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.payme.server_payment.service.StripeWebhookService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("payme/api/payment/stripe/webhook")
+@RequiredArgsConstructor
+public class StripeWebhookController {
+
+    private final StripeWebhookService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Void> handleStripeWebhookController(@RequestBody String payload, @RequestHeader("Stripe-Signature") String signature) {
+
+        service.handleStripeWebhookService(payload, signature);
+        System.out.println("Stripe webhook received successfully");
+        return ResponseEntity.ok().build();
+    } 
+}
