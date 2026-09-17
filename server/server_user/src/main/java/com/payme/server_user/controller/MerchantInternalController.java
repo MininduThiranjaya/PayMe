dd67ce38b170c9b59d0d05e68a5e253a06c3df5e
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-17 18:21:39 +0530

    feat: add merchant internal controller

---
+++ server/server_user/src/main/java/com/payme/server_user/controller/MerchantInternalController.java
@@ -0,0 +1,32 @@
+package com.payme.server_user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.payme.server_user.services.MerchantInternalService;
+import com.payme.server_user.DTO.req_dto.StripeWebhookUpdateAcc_req_dto;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("payme/api/user/internal/merchant")
+@RequiredArgsConstructor
+public class MerchantInternalController {
+    
+    private final MerchantInternalService service;
+
+    @PutMapping("/update/stripe-status")
+    public ResponseEntity<Void> updateStripeStatusController(@RequestBody StripeWebhookUpdateAcc_req_dto data) {
+
+        service.updateStripeStatuService(data);
+        return ResponseEntity.ok().build();
+    }
+}
