51a692aaa420b7225882b3efaa2e3018fc97c71a
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-19 16:20:28 +0530

    feat: add new endpoints

--- client/lib/config/ApiEndpoints.dart
+++ client/lib/config/ApiEndpoints.dart
@@ -1,8 +1,9 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String userRegister = 'user/user-reg';
-  static const String merchantRegister = 'user/merchant-reg';
+  static const String userRegister = 'user/reg/customer';
+  static const String merchantRegister = 'user/reg/merchant';
   static const String login = 'user/login';
   static const String me = 'user/me';
+  static const String setNewOrder = 'order/create-order';
 }
