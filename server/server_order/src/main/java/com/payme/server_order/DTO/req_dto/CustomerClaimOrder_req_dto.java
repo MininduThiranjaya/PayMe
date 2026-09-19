835ada8c3f773df1ee35c631b17c26b047f91448
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-19 16:27:40 +0530

    fix: resolve bug in class name not setup in dto

--- server/server_order/src/main/java/com/payme/server_order/DTO/req_dto/CustomerClaimOrder_req_dto.java
+++ server/server_order/src/main/java/com/payme/server_order/DTO/req_dto/CustomerClaimOrder_req_dto.java
@@ -9,8 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class 
- {
+public class CustomerClaimOrder_req_dto {
     
     @NotNull(message = "Order id is required")
     @Positive(message = "Order id must be greater than 0")
