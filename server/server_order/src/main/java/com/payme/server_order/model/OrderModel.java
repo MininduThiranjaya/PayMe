15e3409bf9b1785250e170b7d92b0a7833d14626
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-19 16:17:40 +0530

    feat: add order status attribute

--- server/server_order/src/main/java/com/payme/server_order/model/OrderModel.java
+++ server/server_order/src/main/java/com/payme/server_order/model/OrderModel.java
@@ -7,9 +7,13 @@ import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.payme.server_order.enums.OrderStatus;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,20 +33,32 @@ public class OrderModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "merchantid", nullable = false)
-    private long merchatId;
-    @Column(name = "customerid", nullable = false)
-    private long customerId;
+    @Column(name = "merchant_nic", nullable = false)
+    private String merchantNic;
+    @Column(name = "customer_nic")
+    private String customerNic;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus status = OrderStatus.CREATED;
+
     @OneToMany(
         mappedBy = "order",
         cascade = CascadeType.ALL,
         orphanRemoval = true
     )
-    private List<OrderItemModel> orderItem = new ArrayList<>();
+    private List<OrderItemModel> orderItem =
+        new ArrayList<>();
     @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false)
+    @Column(
+        name = "created_at",
+        nullable = false,
+        updatable = false
+    )
     private LocalDateTime createdAt;
     @UpdateTimestamp
-    @Column(name = "updated_at", nullable = false)
+    @Column(
+        name = "updated_at",
+        nullable = false
+    )
     private LocalDateTime updatedAt;
 }
