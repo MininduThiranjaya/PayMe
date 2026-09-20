b779e8c93a4b3bf65fb4e6968c90d55d78a87a36
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-20 11:56:11 +0530

    feat: set up on delete cascade on db table

--- server/server_user/src/main/java/com/payme/server_user/model/MerchantModel.java
+++ server/server_user/src/main/java/com/payme/server_user/model/MerchantModel.java
@@ -3,14 +3,18 @@ package com.payme.server_user.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.payme.server_user.enums.MerchantStatus;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,23 +24,31 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@OnDelete(
+    action = OnDeleteAction.CASCADE
+)
 public class MerchantModel extends UserModel {
 
-    @OneToMany(mappedBy = "merchant", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(
+        mappedBy = "merchant",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
+    @OnDelete(
+        action = OnDeleteAction.CASCADE
+    )
     private List<ShopModel> shopNames = new ArrayList<>();
     private String stripeAccountId;
     private Boolean chargesEnabled;
     private Boolean payoutsEnabled;
     private Boolean detailsSubmitted;
     @Enumerated(EnumType.STRING)
-    private MerchantStatus merchantStatus =
-            MerchantStatus.pending_stripe_onboarding;
-
+    private MerchantStatus merchantStatus = MerchantStatus
+        .pending_stripe_onboarding;
     public void addShopDetails(ShopModel shop) {
         shopNames.add(shop);
         shop.setMerchant(this);
     }
-
     public List<ShopModel> getShopDetails() {
         return shopNames;
     }
--- server/server_user/src/main/java/com/payme/server_user/model/ShopModel.java
+++ server/server_user/src/main/java/com/payme/server_user/model/ShopModel.java
@@ -9,26 +9,32 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="shop_names")
+@Table(name = "shop_names")
 @Getter
 @Setter
 @NoArgsConstructor
 public class ShopModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(
+        strategy =
+            GenerationType.IDENTITY
+    )
     private Long id;
     @Column(name = "shopname", nullable = false)
     private String shopName;
     @Column(name = "address", nullable = false)
     private String address;
-
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "merchant_id", nullable = false)
+    @JoinColumn(
+        name = "merchant_id",
+        nullable = false
+    )
     private MerchantModel merchant;
 }
